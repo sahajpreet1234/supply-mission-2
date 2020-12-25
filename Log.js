977833356d1e182420a6411457564802aff47504
sahajpreet1234 <71757240+sahajpreet1234@users.noreply.github.com>
2020-12-25 12:31:30 +0530

    Add files via upload

---
+++ Log.js
@@ -0,0 +1,15 @@
+class Log {
+  constructor(x,y,width,hight){
+  var options = {
+    restitution: 0.8
+  }
+  this.body=Bodies.rectangle(x,y,width,hight);
+  World.add(world.this.bodies);
+  }
+  display(){
+    var pos =this.body.position
+    rectMode(CENTER);
+     fill(255);
+     rect(pos.x,pos.y,this.width,this.hight);
+  }
+}
